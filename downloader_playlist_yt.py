9dd049ca748af0c86af896cf4f03b4b475832e5a
juniofreitas <juniofreitas@gmail.com>
2024-07-05 13:11:13 -0400

    Remoção de comentários e código desnecessários

--- downloader_playlist_yt.py
+++ downloader_playlist_yt.py
@@ -42,28 +42,3 @@ if __name__ == "__main__":
     file = sys.argv[1]
     main(file)
     print("All playlist downloaded successfully! 🎉")
-
-
-# link = input("Enter YouTube Playlist URL: ✨")
-
-# yt_playlist = Playlist(link)
-
-# folderName = make_alpha_numeric(yt_playlist.title)
-# os.mkdir(folderName)
-
-# totalVideoCount = len(yt_playlist.videos)
-# print("Total videos in playlist: 🎦", totalVideoCount)
-
-# for index, video in enumerate(yt_playlist.videos, start=1):
-#     print("Downloading:", video.title)
-#     video_size = video.streams.get_highest_resolution().filesize
-#     print("Size:", video_size // (1024**2), "🗜 MB")
-#     # video.streams.get_highest_resolution().download(output_path=folderName)
-#     video.streams.filter(only_audio=True, mime_type="audio/mp4").order_by(
-#         "abr"
-#     ).desc().first().download(output_path=folderName)
-
-#     print("Downloaded:", video.title, "✨ successfully!")
-#     print("Remaining Videos:", totalVideoCount - index)
-
-# print("All videos downloaded successfully! 🎉")
